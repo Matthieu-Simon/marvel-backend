92bd0ef6a56e51a6d9d1357398037f066f35ae43
Matthieu-Simon <simon.matthieu89@gmail.com>
2024-09-05 17:03:46 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const apiKey = process.env.MARVEL_API_KEY;
+
+app.get('/characters',  async (req, res) => {
+    try {
+        // console.log(req.query);
+        const name = req.query.name || "";
+        const limit = req.query.limit || 100;
+        const skip = req.query.skip || 0;
+        
+        const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${apiKey}&limit=${limit}&name=${name}&skip=${skip}`);
+
+        // console.log(response.data);
+        res.json(response.data)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/comics', async (req, res) => {
+    try {
+        const limit = req.query.limit || 100;
+
+        const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apiKey}&limit=${limit}`);
+        // console.log(response.data);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/comics/:characterId', async (req, res) => {
+    try {
+        // console.log(req.params.characterId);
+        const characterId = req.params.characterId;
+        // console.log("result", characterId);
+        
+        const response = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${apiKey}`);
+        // console.log(response.data);
+        res.json(response.data)
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+app.all('*', (req, res) => {
+    res.status(500).json({ message: error.message });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server started");
+});
